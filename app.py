9b016c75cab838a03e4481c8b10b698aaa7c8917
Abnormal Ghoul <151715344+AbnormalGhoul@users.noreply.github.com>
2025-11-02 21:01:41 -0800

    CI/CD Workflow Redo

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return (
-    "<h1> Hello from Flask CI/CD Demo v1!</h1>"
-    "<p>Automatic Build → Test → Deploy via GitHub Actions.</p>"
+        "<h1>Hello from Flask CI/CD Demo v1!</h1>"
+        "<p>Automatic Build → Test → Deploy via GitHub Actions.</p>"
     )
 
 
